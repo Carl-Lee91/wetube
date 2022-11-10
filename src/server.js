d448d4394af10e291a3992efcb845cb6639dd797
Carl-Lee91 <112924805+Carl-Lee91@users.noreply.github.com>
2022-11-10 12:42:05 +0900

    11.10 lec

--- src/server.js
+++ src/server.js
@@ -1,24 +1,15 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouters";
+import videoRouter from "./routers/videoRouters";
+import userRouter from "./routers/userRouters";
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-
-const handleHome = (req, res) => {
-    return res.end("<h1>I am EndWare!</h1>");
-};
-
-const handleProtected = (req,res) => {
-    return res.send("Welcome to the private lounge")
-}
-
 app.use(logger);
-app.get("/", handleHome);
-app.get("/protected", handleProtected);
 
-const handleListening = () =>
- console.log(`Server listnening on port http://localhost:${PORT}`);
-
-app.listen(PORT, handleListening);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
