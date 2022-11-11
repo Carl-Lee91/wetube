744a321259a330698f16e429419f0e92586bcd30
Carl-Lee91 <112924805+Carl-Lee91@users.noreply.github.com>
2022-11-11 14:54:52 +0900

    11.11 lec

--- src/routers/videoRouters.js
+++ src/routers/videoRouters.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { watch, edit } from "../controllers/videoControllers";
+import { upload, see, edit, deleteVedio } from "../controllers/videoControllers";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/watch", watch);
-videoRouter.get("/edit", edit);
+videoRouter.get("/upload", upload);
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVedio);
 
 export default videoRouter;
