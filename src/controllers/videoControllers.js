f0466d7cc905211fa6f44a6f6fd29ed16a9d9e16
Carl-Lee91 <112924805+Carl-Lee91@users.noreply.github.com>
2022-11-16 15:11:09 +0900

    11.16 lec

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,6 +1,6 @@
-export const trending = (req, res) => res.send("<h1>Home Page Videos</h1>");
-export const see = (req, res) => res.send("See");
-export const edit = (req, res) => res.send("Edit");
+export const trending = (req, res) => res.render("home", {pageTitle: "Home"});
+export const see = (req, res) => res.render("watch", {pageTitle: "Watch"});
+export const edit = (req, res) => res.render("edit", {pageTitle: "Edit"});
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVedio = (req, res) => res.send("Delete Video");
