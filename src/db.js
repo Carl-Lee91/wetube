ed993ecc37318535d272b7bb0fb9c0aa59947b91
Carl-Lee91 <112924805+Carl-Lee91@users.noreply.github.com>
2022-12-26 15:40:02 +0900

    12.26

---
+++ src/db.js
@@ -0,0 +1,13 @@
+import mongoose, { mongo } from "mongoose";
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect("mongodb://127.0.0.1:27017/WeTube", {useNewUrlParser: true}, {useUnigiedTopology: true});
+
+const db =  mongoose.connection;
+
+const handleOpen = () => console.log("Connected to DB")
+const handleError = (error) => console.log("DB Error", error);
+
+db.on("error", handleError);
+db.once("open", handleOpen);
