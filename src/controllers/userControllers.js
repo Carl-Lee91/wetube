d7e08df2af2b63e7f1a896c3c03b1093c2bfee4f
Carl-Lee91 <112924805+Carl-Lee91@users.noreply.github.com>
2023-01-06 14:55:56 +0900

    01.06

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -3,9 +3,9 @@ import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", {pageTitle:"Join"});
 export const postJoin = async(req, res) => {
-    const {name, email, username, password1, password2, location} = req.body;
+    const {name, email, username, password, password2, location} = req.body;
     const pageTitle = "Join";
-    if(password1 !== password2){
+    if(password !== password2){
         return res.status(400).render("Join", {pageTitle, errorMessage:"Password confirmation does not match."}); 
     }
     const exists = await User.exists({$or:[{username}, {email}]});
@@ -17,7 +17,7 @@ export const postJoin = async(req, res) => {
         name,
         email,
         username, 
-        password1, 
+        password, 
         password2,
         location,
     });
@@ -33,11 +33,13 @@ export const postLogin = async(req, res) => {
     const user = await User.findOne({username});
     if(!user){
         return res.status(400).render("login", {pageTitle, errorMessage: "An account with this username does not exists."})
-    }
+    } 
     const fine = await bcrypt.compare(password, user.password);
     if(!fine){
         return res.status(400).render("login", {pageTitle, errorMessage: "Wrong password"})
     }
+    req.session.loggedIn = true;
+    req.session.user = user;
     return res.redirect("/");
 }
 export const edit = (req, res) => res.send("Edit User");
