2ceac04e8b2f99491898966393bedaf7322fdf10
Carl-Lee91 <112924805+Carl-Lee91@users.noreply.github.com>
2023-01-09 23:11:02 +0900

    01.09

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose, { mongo } from "mongoose";
 
 mongoose.set("strictQuery", false);
 
-mongoose.connect("mongodb://127.0.0.1:27017/WeTube", {useNewUrlParser: true}, {useFindAndModify: false},{useUnigiedTopology: true},);
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true}, {useFindAndModify: false},{useUnigiedTopology: true},);
 
 const db =  mongoose.connection;
 
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootrouter from "./routers/rootrouter";
 import videoRouter from "./routers/videoRouters";
 import userRouter from "./routers/userRouters";
@@ -16,9 +17,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
     session({
-        secret: "Hello!",
-        resave: true,
-        saveUninitialized: true,
+        secret: process.env.COOKIE_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({mongoUrl: process.env.DB_URL})
 })
 );
 
