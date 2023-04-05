559f12e4b940ebf5d10733dfd4ee10be762e4698
Carl-Lee91 <112924805+Carl-Lee91@users.noreply.github.com>
2023-04-05 19:30:59 +0900

    0405

---
+++ src/client/js/recorder.js
@@ -0,0 +1,48 @@
+const startBtn = document.getElementById("startBtn");
+const audio = document.getElementById("preview");
+
+let stream;
+let recorder;
+let audioFile;
+
+const handleDownload = () => {
+  const a = document.createElement("a");
+  a.href = audioFile;
+  a.download = "MyRecording!.wmv";
+  document.body.appendChild(a);
+  a.click();
+};
+
+const handleStop = () => {
+  startBtn.innerText = "Download Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleDownload);
+  recorder.stop();
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+  recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (event) => {
+    audioFile = URL.createObjectURL(event.data);
+    audio.srcObject = null;
+    audio.src = audioFile;
+    audio.loop = true;
+  };
+  recorder.start();
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: false,
+  });
+  audio.srcObject = stream;
+  audio.play();
+};
+
+init();
+
+startBtn.addEventListener("click", handleStart);
--- webpack.config.js
+++ webpack.config.js
@@ -3,36 +3,37 @@ const path = require("path");
 
 module.exports = {
   entry: {
-    main:"./src/client/js/main.js",
-    videoPlayer: "./src/client/js/videoPlayer.js"
+    main: "./src/client/js/main.js",
+    videoPlayer: "./src/client/js/videoPlayer.js",
+    recorder: "./src/client/js/recorder.js",
   },
-  mode: 'development',
-  watch:true,
-  plugins: [new MiniCssExtractPlugin({
-    filename:"css/styles.css"
-  })],
+  mode: "development",
+  watch: true,
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/styles.css",
+    }),
+  ],
   output: {
-    filename: 'js/[name].js',
+    filename: "js/[name].js",
     path: path.resolve(__dirname, "assets"),
     clean: true,
   },
   module: {
     rules: [
-        {
-            test:/\.js$/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                  presets: [
-                    ['@babel/preset-env', { targets: "defaults" }]
-                  ]
-                }
-              }
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
         },
-        {
-          test: /\.scss$/,
-          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
-        }
-    ]
-  }
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
 };
